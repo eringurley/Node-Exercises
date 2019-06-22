49953736db366ea8b02cea470467f2b1e286233f
Erin Gurley <erinmgurley@gmail.com>
2019-06-21 22:51:43 -0700

    creating a form in the requestHandler

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,12 +2,26 @@ var exec = require("child_process").exec;
 
 function start(response) {
     console.log("Request handler 'start' was called.");
+
+var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+
 }
-    exec("ls -lah", function (error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
-        response.end();
-    });
 
 function upload(response) {
     console.log("Request handler 'upload' was called.");
